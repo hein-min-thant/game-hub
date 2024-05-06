31e6b47fae2721a68d3d446273749aaeab9a7dba
Hein Min Thant <heinminthant259@gmail.com>
2024-05-06 15:50:04 +0630

    Sort the games

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -5,16 +5,17 @@ import { Platform } from "../hooks/useGames";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({ onSelectPlatform }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatform();
 
   if (error) return null;
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platform
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
