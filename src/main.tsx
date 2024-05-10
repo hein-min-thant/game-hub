ad4b080a1b7d32e20c20322e5eb4affabbf5b791
Hein Min Thant <heinminthant259@gmail.com>
2024-05-10 11:45:51 +0630

    refactor genres with reeact-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import theme from "./theme.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
