2b77b228a707b5f9b76d609f81b3c0455ad5a3bf
Hein Min Thant <heinminthant259@gmail.com>
2024-05-12 21:59:44 +0630

    fetch game detail

---
+++ src/hooks/useGame.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClinet from "../services/api-client";
+import { Game } from "./useGames";
+
+const apiClient = new APIClinet<Game>("/games");
+
+const useGame = (slug: string) => {
+  return useQuery({
+    queryKey: ["game", slug],
+    queryFn: () => apiClient.get(slug),
+  });
+};
+
+export default useGame;
--- src/routes.tsx
+++ src/routes.tsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: "game/:id",
+        path: "games/:slug",
         element: <GameDetailPage />,
       },
     ],
