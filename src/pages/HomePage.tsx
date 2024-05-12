03de87c5f86b14d146b395a2d77efd673e307757
Hein Min Thant <heinminthant259@gmail.com>
2024-05-12 22:10:20 +0630

    padding again

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -24,7 +24,7 @@ const HomePage = () => {
         </GridItem>
       </Show>
       <GridItem area="main" overflowY="auto">
-        <Box paddingLeft={2}>
+        <Box>
           <GameHeading />
           <HStack spacing={5} marginBottom={5}>
             <PlatformSelector />
