059c17899eb451b5af3bbb45233605faec4995ed
Hein Min Thant <heinminthant259@gmail.com>
2024-05-13 14:05:40 +0630

    Refactor entities

--- src/components/GameAttributes.tsx
+++ src/components/GameAttributes.tsx
@@ -1,4 +1,4 @@
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import DefinitionItems from "./DefinitionItems";
 import CriticScore from "./CriticScore";
