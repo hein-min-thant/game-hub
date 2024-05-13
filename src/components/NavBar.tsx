12ef756d373481db92819c5db49be51139638fa6
Hein Min Thant <heinminthant259@gmail.com>
2024-05-13 14:29:32 +0630

    "logo"

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <HStack padding={"10px"}>
-      <Link to="/" style={{ width: "100%" }}>
+      <Link to="/" style={{ width: "130px" }}>
         <Image src={logo} boxSize="60px" objectFit="cover" />
       </Link>
       <SearchInput />
